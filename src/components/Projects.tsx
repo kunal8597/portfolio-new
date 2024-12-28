a3c92abf226143b28b2984954da1a83666e6350d
Kunal Mathur <93191673+kunal8597@users.noreply.github.com>
2024-12-28 20:44:47 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import { FaGithub } from "react-icons";
+import { FaGithub } from "react-icons/fa6";
 import Image from 'next/image';
 import Link from "next/link";
 import Journing from "/public/images/journing.png"
